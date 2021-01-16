0e5384eb13a101af2aad04892e92e9f16f155b24
Timits <will4f21@yahoo.com>
2021-01-16 03:38:02 -0800

    create tododiv with todo's complete/delete buttons append to UL

--- app.js
+++ app.js
@@ -12,4 +12,33 @@ function addTodo (event) {
     // prevent form from submitting or refreshing
     event.preventDefault();
 
+    // todo-div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    // create-li --append
+    const newTodo = document.createElement('li');
+    newTodo.classList.add('todo-item');
+    newTodo.innerText = 'test';
+    todoDiv.appendChild(newTodo);
+
+    // Check Mark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    // Check Trash Button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('complete-btn');
+    todoDiv.appendChild(trashButton);
+
+    // Append todoDiv
+    todoList.appendChild(todoDiv);
+
+
+
+    console.log(todoDiv);
+
 }
